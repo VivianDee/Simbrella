0c4578c8232441718d6b06a93153685e4483e28f
VivianDee <vdagbue@gmail.com>
2024-09-18 05:49:12 +0100

    [add]: Folder Structure and Authentication

---
+++ backend/app/Modules/AuthenticationModule/A.uthenticationModule.php
@@ -0,0 +1,23 @@
+<?php 
+
+namespace App\Modules;
+
+use Illuminate\Http\Request;
+
+class AuthenticationModule
+{
+    // public $signInService;
+    // public $accountCreationService;
+    // public $changePasswordService;
+
+    // public function __construct(SignInService $signInService, AccountCreationService $accountCreationService, ChangePasswordService $changePasswordService) {
+    //     $this->signInService = $signInService;
+    //     $this->accountCreationService = $accountCreationService;
+    //     $this->changePasswordService = $changePasswordService;
+    // }
+
+    public function login(Request $request) {
+
+    }
+
+}
